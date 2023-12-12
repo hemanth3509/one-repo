19a24afc4a13e49b4e9a0487194eb1bf2cccd251
hemanth3509 <saihemanth2230@gmail.com>
2023-12-12 17:06:48 +0530

    My script edit file

---
+++ complete sh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#This is the comment section and first is in shell script mandate first line
+
+USERNAME=$1
+PASSWORD=$2
+echo "Please enter your username"
+read username
+echo "Please enter your password"
+read password
+
+echo "your user name is $username and password is $password"

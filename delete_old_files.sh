4524c077d94c7948e25cc1c5fb359d1e720c5b7c
hemanth3509 <saihemanth2230@gmail.com>
2023-12-19 12:43:37 +0530

    robo shell script

--- delete_old_files.sh
+++ delete_old_files.sh
@@ -5,18 +5,17 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo " Script started $OPTARG"
+echo " Script started "
 
 help(){
-    echo -e " Please find the below usage of the script \n"
-    echo -e " -s <source dir> \n "
-    echo -e " -a <Archive> or <Delete> \n "
-    echo -e " -d <destination > \n "
-    echo -e " -t <time> \n "
+    echo " USAGE : { sh old-logs.sh -s <source-dir> -a <archive|delete> -d <destination> -t <day> -m <memory-in-mb> } "
+    echo " -s <source dir> "
+    echo " -a <Archive> or <Delete>"
+    echo " -d <destination >"
+    echo " -t <time>"
 }
 
 action (){
-    echo "inside actions func"
     if [ ! -d "$SOURCE_DIR" ]  # ! denotes opposite
 then
     echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
@@ -29,12 +28,6 @@ then
      then
     echo -e "$G destination exits to archive $N"
     fi
-  # if [ ! -d "$DESTINATION" ]
-  #  then
-  #  echo -e "$Y destination directory doesn't exists , Hence creating new one $N"
-  #  else 
-  #  echo -e "$G destination exits $N"
-  #  fi
    fi
     if [ "$archive" == "delete" ]
     then 
@@ -52,53 +45,34 @@ fi
 
 options(){
     OPTSTRING=":s:a:d:t:"
-    echo "inside options $1 "
-while getopts ${OPTSTRING} opt;
-do
-case ${opt} in
-s) 
-    SOURCE_DIR=$OPTARG ; 
-    echo "source dir $SOURCE_DIR ";;
-a)  archive=$OPTARG ; 
-    echo " archive $archive" ;;
-d)  DESTINATION=$OPTARG ; 
-    echo " destination is $DESTINATION" ;;
-t) 
-    TIME=$OPTARG; 
-    echo " time is $TIME" ;;
-:) 
-    echo " in : please pass the arguments";
-   help; 
-    exit 1 ;;
-?)  help;
-    exit 1 ;;
-esac
-done
-shift $((OPTIND -1))
+        while getopts ${OPTSTRING} opt;
+            do
+            case ${opt} in
+                s)  SOURCE_DIR=$OPTARG ;; 
+                a)  archive=$OPTARG ;;
+                d)  DESTINATION=$OPTARG ;; 
+                t)  TIME=$OPTARG ;; 
+                :) 
+                    echo " in : please pass the arguments";
+                help; 
+                    exit 1 ;;
+                ?)  help;
+                    exit 1 ;;  
+            esac
+            done
+        shift $((OPTIND -1))
 }
 
-echo "Before calling options"
-echo " $1"
 if [ "$1" != "-s" ]
-then
-help
-else
-options "$@"
+    then
+        help
+    else
+        options "$@"
 fi
-
-echo " after options calling" 
 
 if [ "$archive" == "archive" ] || [ "$archive" == "delete" ]
-then
-action "$archive" "$DESTINATION"
-else
-help
+    then
+    action "$archive" "$DESTINATION"
+    else
+    help
 fi
-
-#FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-
-#while IFS= read -r line
-#do
-#    echo "Deleting file: $line"
-#    rm -rf $line
-# done <<< $FILES_TO_DELETE

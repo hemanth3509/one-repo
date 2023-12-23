c52b66977f371dc6fb3fda07088758bbfc2ecb40
hemanth3509 <saihemanth2230@gmail.com>
2023-12-23 13:14:15 +0530

    robo shell script

--- delete_old_files.sh
+++ delete_old_files.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 help(){
     echo " ERROR in passing the arguments "
-    echo " USAGE : { sh old-logs.sh -s <source-dir> -a <archive|delete> -d <destination> -t <day> -m <memory-in-mb> } "
+    echo " USAGE : { sh old-logs.sh -s <source-dir> -a <archive|delete> -d <destination dir> -t <days> -m <memory-in-mb> } "
     echo " -s <source dir> "
     echo " -a <Archive> or <Delete>"
     echo " -d <destination >"
@@ -15,70 +15,69 @@ help(){
 }
 
 action (){
-    if [ ! -d "$SOURCE_DIR" ]  # ! denotes opposite
-then
-    echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
-    exit 1
-    else
-    if [ "$archive" == "archive" ] && [ ! -d "$DESTINATION" ]
+    if [ ! -d "$SOURCE_DIR" ] 
     then
-    echo -e "$R Please provide valid destination directory $N"
-    exit 1
+      echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
+      exit 1
     else
-     if [ "$archive" == "archive" ] && [ -d "$DESTINATION" ]
+    if [ "$archive" == "archive" ] && [ ! -d "$DESTINATION" ] # When archive specified then destination is mandate
+      then
+       echo -e "$R Please provide valid destination directory $N"
+       help
+       exit 1
+    else
+    if [ "$archive" == "archive" ] && [ -d "$DESTINATION" ] # When bothe archive and destination is given
      then
-    echo -e "$G destination exits to archive $N" # Replace with zip command
-    FILES_TO_ARCHIVE=$(find $SOURCE_DIR -type f -mtime +$TIME -name "*.log")
-    if [ $? != 0 ]
-           then 
-           echo -e " $R ERROR $N"
-           exit 1
-           else  
-             if [ -n "$FILES_TO_ARCHIVE" ]
-             then 
-        while IFS= read -r line
-            do
-                echo "Archiving files : $line"
-                zip -r "$DESTINATION/$(basename "$line").zip" $line
-                if [ $? == 0 ]
-                then
-                rm -rf $line
-                else
-                echo " Error in archiving"
+        echo -e "$G destination exits to archive $N"
+        FILES_TO_ARCHIVE=$(find $SOURCE_DIR -type f -mtime +$TIME -name "*.log") # Finding files to archive
+            if [ $? != 0 ] # checks if find executed successfully or not to proceed further
+                then 
+                echo -e " $R ERROR in finding the files $N" 
                 exit 1
-                fi
-        done <<< $FILES_TO_ARCHIVE
-             else
-             echo -e "$Y No file to archive $N"
+                else  
+                if [ -n "$FILES_TO_ARCHIVE" ]  # If files are found and available to archive
+                then 
+                    while IFS= read -r line
+                    do
+                    echo "Archiving files : $line"
+                    zip -r "$DESTINATION/$(basename "$line").zip" $line
+                    if [ $? == 0 ]
+                        then
+                            rm -rf $line
+                        else
+                            echo " Error in archiving"
+                            exit 1
+                    fi
+                    done <<< $FILES_TO_ARCHIVE
+                else
+                echo -e "$Y No file to archive $N"  # When no files exists to archive
                 fi
             fi
-    fi
+        fi
    fi
     if [ "$archive" == "delete" ]
     then 
-        echo -e "$G Source directory exists $SOURCE_DIR please delete $N"
-        if [ -z "$TIME" ]
-                then 
-        echo "please provide the time to delte the logs"
-        echo " If date is provided please add + before time "
-        exit 1
+        if [ -z "$TIME" ] # Checks if time arg is provided or not
+            then 
+              echo "please provide the time to delte the logs"
+              help
+              exit 1
         else
         FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +$TIME -name "*.log")
-        echo "$FILES_TO_DELETE"
-           if [ $? != 0 ]
+           if [ $? != 0 ]  # Checks if find executed sucessfully or not 
            then 
-           echo -e " $R ERROR $N"
+           echo -e " $R ERROR in finding the files $N"
            exit 1
            else  
-             if [ -n "$FILES_TO_DELETE" ]
+             if [ -n "$FILES_TO_DELETE" ]  # If files are availble to delete
              then 
-        while IFS= read -r line
-            do
-                echo "Deleting file: $line"
-                rm -rf $line
-        done <<< $FILES_TO_DELETE
+                while IFS= read -r line
+                do
+                    echo "Deleting file: $line"
+                    rm -rf $line
+                done <<< $FILES_TO_DELETE
              else
-             echo -e "$Y No file to delete $N"
+                    echo -e "$Y No files to delete $N"  # When no files are there
                 fi
             fi
         fi
@@ -97,7 +96,7 @@ options(){
                 t)  TIME=$OPTARG ;; 
                 :) 
                     echo " in : please pass the arguments";
-                help; 
+                    help; 
                     exit 1 ;;
                 ?)  help;
                     exit 1 ;;  
